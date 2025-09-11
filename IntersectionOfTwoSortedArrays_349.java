6962616f33b396ad6403a9be15d7dcb3d3e08623
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2025-09-11 19:52:34 +0530

    Create IntersectionOfTwoSortedArrays_349.java

---
+++ IntersectionOfTwoSortedArrays_349.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        ArrayList<Integer>al=new ArrayList<>();
+        for(int i=0;i<nums1.length;i++)
+        {
+            for(int j=0;j<nums2.length;j++)
+            {
+                if(nums1[i]==nums2[j])
+                {
+                    if(!al.contains(nums1[i]))
+                    {
+                        al.add(nums1[i]);
+                        break;
+                    }
+                }
+            }
+        }
+        int arr[]=new int[al.size()];
+        for(int i=0;i<al.size();i++)
+        {
+            arr[i]=al.get(i);
+        }
+        return arr;
+    }
+}
