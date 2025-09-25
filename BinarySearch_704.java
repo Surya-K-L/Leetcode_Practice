3698e586f5020c284a6c44c6060b064818b13a99
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2025-09-25 09:48:40 +0530

    Create BinarySearch_704.java

---
+++ BinarySearch_704.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int s=0;
+        int e = nums.length-1;
+        if(nums.length==1 && nums[0]==target)
+            return 0;
+        while(s<=e)
+        {
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target)
+                return mid;
+            if(nums[mid]<target)
+            {
+                s=mid+1;
+            }
+            else
+            {
+                e=mid-1;
+            }
+        }
+        return -1;
+    }
+}
